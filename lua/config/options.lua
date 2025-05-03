5bae90b819af0ff577014e02c1b087c886f89302
Gunter Kluge <gunter.kluge@t-online.de>
2025-05-03 23:08:14 +0200

    ein erster schritt der Modularisierung

---
+++ lua/config/options.lua
@@ -0,0 +1,40 @@
+local M = {}
+
+M.options = {
+  number = true,
+  mouse = 'a',
+  showmode = false,
+  clipboard = 'unnamedplus',
+  breakindent = true,
+  undofile = true,
+  ignorecase = true,
+  smartcase = true,
+  signcolumn = 'yes',
+  updatetime = 250,
+  timeoutlen = 300,
+  splitright = true,
+  splitbelow = true,
+  list = true,
+  listchars = { tab = '» ', trail = '·', nbsp = '␣' },
+  inccommand = 'split',
+  cursorline = true,
+  scrolloff = 10,
+  confirm = true,
+}
+function M.setup()
+  for key, value in pairs(M.options) do
+    if key == 'clipboard' then
+      vim.schedule(function()
+        vim.opt[key] = value
+      end)
+    else
+      vim.opt[key] = value
+    end
+  end
+end
+function M.helper()
+  if 4 > 2 then
+    print 'Geschafft'
+  end
+end
+return M
